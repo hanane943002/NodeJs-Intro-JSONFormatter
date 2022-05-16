1fdd01c171f6f63d5226e1928374a084c3de73e8
hraihan <rhn.hanane@gmail.com>
2022-05-16 17:34:56 +0200

    commit " faire fonction avec arguments"

--- my_magical_program.js
+++ my_magical_program.js
@@ -1,8 +1,17 @@
-console.log("hello world !")
 const fs = require('fs');
-const obj = fs.readFileSync("./my_magical_program.json", {encoding:"utf8", flag:"r"});
-const data = JSON.parse(obj);
-const newdata = JSON.stringify(data,null,2);
-console.log(newdata);
-const outpout = fs.writeFileSync("outpout.json",newdata);
+let myArgs = process.argv.slice(2);
+myArgs2 = myArgs[1]
 
+
+
+function inputArgs(myArgs, myArgs2){
+    console.log(myArgs);
+    const obj = fs.readFileSync(myArgs, {encoding:"utf8", flag:"r"});
+    const data = JSON.parse(obj);
+    const newdata = JSON.stringify(data,null,2);
+    console.log(newdata);
+    const output = fs.writeFileSync(myArgs2, newdata);
+
+
+}
+inputArgs(myArgs[0], myArgs2)
